ae74433a0226cf273a06b376a798697ef71b3347
lukasyang <731180001@qq.com>
2022-11-15 23:30:56 +0800

    ASM插桩打印函数耗时

---
+++ app/src/main/java/com/xiaozhezhe/asmdemo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.xiaozhezhe.asmdemo;
+
+public class HelloWorld {
+
+    public void sayHello() {
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
